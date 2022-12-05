ea1b88dee9c6d7434e721d38098c4cf273228d85
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-12-05 14:55:12 +0100

    Implement in-place stack

---
+++ utils/ip_stack.c
@@ -0,0 +1,31 @@
+#include "ip_stack.h"
+
+void _grow(ip_stack* s){
+    s->content = realloc(s->content, 2*s->_max_size);
+    s->_max_size *= 2;
+}
+
+ip_stack get_empty(int max_size){
+    ip_stack s;
+    s.content = malloc(max_size*sizeof(int));
+    s.size = 0;
+    return s;
+}
+
+void push(ip_stack* s, int n){
+    if (s->size == s->_max_size)
+        _grow(s);
+    s->content[s->size++] = n;
+}
+
+int pop(ip_stack* s){
+    if(s->size == 0){
+        printf("Error : pop from empty stack");
+        return -1;
+    }
+    return s->content[--s->size];
+}
+
+void free_stack(ip_stack* s){
+    free(s->content);
+}
---
+++ utils/ip_stack.h
@@ -0,0 +1,27 @@
+#ifndef IP_STACK_H
+#define IP_STACK_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Defines an in-place stack implemented using arrays
+*/
+struct ip_stack
+{
+    int* content;
+    int size;
+    int _max_size;
+
+} typedef ip_stack;
+
+void _grow(ip_stack* s);
+ip_stack get_empty(int max_size);
+
+void push(ip_stack* s, int n);
+int pop(ip_stack* s);
+
+void free_stack(ip_stack* s);
+
+
+#endif
