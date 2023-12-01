ffe57d6855296eba9050bd0002d90d1e1b55db0c
gbrivady <12777915+gbrivady@users.noreply.github.com>
2023-12-01 09:31:52 +0100

    Add cleaned day 1

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ Language for this year will be `rust`! To run day `xx`, simply do `cargo run --b
 List of every day, with every challenge. The mark is my opinion on my solution : 1 is minimal effort, working solution, 2 is for a more fancy one, and 3 is when I tried to clean the code afterwards and do some fancy stuff.
 Time is `hh:mm:ss`, and using a separate timer - as `UTC-5` is a bit rough.
 
-| N° | Status | T Part 1 | T Part 2 | T1+T2 | Remarks |
+| N° | Status | T Part 1 | T Part 2 | T1+T2 | Self-grade |
 | :--: | :------: | :--------: | :--------: | :-----: | -------: |
-|1 |✅|•|•|•|•|
+|1 |✅| 00:03:55 | 00:10:50 | 00:14:45 | 2 |
 |2 |❓|•|•|•|•|
 |3 |❓|•|•|•|•|
 |4 |❓|•|•|•|•|
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,48 @@
+use std::fs::read_to_string;
+
+const DIGIT_STR: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
 fn main() {
-    println!("Hello AOC 2023");
+    let mut result: (u32, u32) = (0, 0);
+
+    for line in read_to_string("input/01").unwrap().lines() {
+        let mut found_first: (bool, bool) = (false, false);
+        let mut cur_digit: (u32, u32) = (0, 0);
+
+        for (i, c) in line.char_indices() {
+            match c.to_digit(10) {
+                Some(digit) => {
+                    cur_digit.0 = digit;
+                    cur_digit.1 = digit;
+                    if !found_first.0 {
+                        found_first.0 = true;
+                        result.0 += 10 * digit;
+                    }
+                    if !found_first.1 {
+                        found_first.1 = true;
+                        result.1 += 10 * digit;
+                    }
+                }
+
+                None => {
+                    for j in 0..9 {
+                        if line[i..].starts_with(DIGIT_STR[j]) {
+                            cur_digit.1 = (j + 1) as u32;
+                            if !found_first.1 {
+                                result.1 += cur_digit.1 * 10;
+                                found_first.1 = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        result.0 += cur_digit.0;
+        result.1 += cur_digit.1;
+    }
+
+    println!("Part 1: {}\nPart 2: {}", result.0, result.1);
 }
